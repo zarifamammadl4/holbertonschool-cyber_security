e6bc9d50bf8d7edac08e0f256add6a2f7ff7fe47
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-12-28 18:41:22 +0000

    Svd

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-domain="$1"
-
-# Subdomainləri ekrana çıxar
-subfinder -d "$domain" -silent | tee /tmp/subs.txt
-
-# Host,IP formatında fayla yaz
-> "$domain.txt"
-while read -r sub; do
-    ip=$(dig +short "$sub" | head -n 1)
-    if [ -n "$ip" ]; then
-        echo "$sub,$ip" >> "$domain.txt"
-    fi
-done < /tmp/subs.txt
-
-rm /tmp/subs.txt
+subfinder -d "$1" -silent
