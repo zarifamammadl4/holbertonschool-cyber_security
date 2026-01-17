7724552d4c88630507f04615e74667fee4251a45
zarifamammadl4 <zarifamammadli4@gmail.com>
2026-01-17 17:48:36 -0500

    ecf

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: 'BEGIN{OFS=","}/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email)/{gsub(/^ +/,"",$2);if($2=="")$2="";k=$1;gsub(" ","",k);if($1~/(Street)/)$2=$2" ";if($1~/(Phone Ext|Fax Ext)/)k=k":";print k,$2}' > "$1.csv"
+whois "$1" | awk -F: 'BEGIN{OFS=", "} /^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email)/{gsub(/^ +/,"",$2);key=$1;if($1~/(Street)/)$2=$2" ";if($1~/(Phone Ext|Fax Ext)/)key=key":";print key,$2}' > "$1.csv"
