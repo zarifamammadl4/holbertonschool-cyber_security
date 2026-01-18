4f71a01eec840293d19f90a5c4f10bfeaf8334ea
zarifamammadl4 <zarifamammadli4@gmail.com>
2026-01-18 12:08:13 -0500

    Dcs

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
-
-# Check argument
 if [ -z "$1" ]; then
     echo "Usage: $0 {xor}HASH"
     exit 1
 fi
-
-HASH="$1"
-
-# Remove {xor} prefix
-CLEAN_HASH="${HASH#\{xor\}}"
-
-# Base64 decode
-DECODED=$(echo "$CLEAN_HASH" | base64 -d 2>/dev/null)
-
-# XOR key used by WebSphere (0x5A)
-KEY=90
-
-RESULT=""
-
-# XOR decode byte by byte (binary safe)
-for (( i=0; i<${#DECODED}; i++ )); do
-    CHAR="${DECODED:$i:1}"
-    BYTE=$(printf '%d' "'$CHAR")
-    XOR_BYTE=$(( BYTE ^ KEY ))
-
-    # Convert number back to character safely
-    printf -v OUT_CHAR '\\x%02x' "$XOR_BYTE"
-    RESULT+="$OUT_CHAR"
-done
-
-echo -e "$RESULT"
+encoded_string="${1#\{xor\}}"
+echo "$encoded_string" | base64 -d | perl -pe '$_ ^= "_" x length'
+echo ""
